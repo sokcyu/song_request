1822b02f77c4b2d5e1034ec0ff8b768e48c63acf
cj스튜디오 <chanjong0123@gmail.com>
2026-07-17 16:15:21 +0900

    Delete firebase-config.js

--- firebase-config.js
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyBxR7FJPfichcVtfwsJ0HrfuULBbdxs1C8",
-  authDomain: "cjstudio-song-request-817e2.firebaseapp.com",
-  projectId: "cjstudio-song-request-817e2",
-  storageBucket: "cjstudio-song-request-817e2.firebasestorage.app",
-  messagingSenderId: "1084570578734",
-  appId: "1:1084570578734:web:3663761f4a819def6fd4bf",
-  measurementId: "G-XV2D1BWG9Z"
-};
